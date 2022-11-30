47291c79b17a7eaeaf712fabff645101c23e2886
taipon55 <git config --global user.email xxxxxx@gmail.com>
2022-12-01 08:57:38 +0900

    メモ新規作成画面作成

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
-  return <MemoEditScreen />;
+  return <MemoCreateScreen />;
 }
