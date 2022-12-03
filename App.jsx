d8fd2380ca06c5a9f0865e5eb9e4d29e38c18690
taipon55 <git config --global user.email xxxxxx@gmail.com>
2022-12-03 12:16:16 +0900

    サインアップ画面作成

--- App.jsx
+++ App.jsx
@@ -3,7 +3,8 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
-  return <LogInScreen />;
+  return <SignUpScreen />;
 }
