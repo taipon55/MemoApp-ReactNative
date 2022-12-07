f5a1095f684fafeae2aa022e5f5b08e8aa34c6b3
taipon55 <Taisei127@outlook.com>
2022-12-07 21:11:50 +0900

    ログアウト機能作成

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import CircleButton from '../components/CircleButton';
 import MemoList from '../components/MemoList';
+import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <MemoList />
