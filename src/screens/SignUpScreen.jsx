6b651e1d0f1b6cef3bab45217de8de80ded13136
taipon55 <Taisei127@outlook.com>
2022-12-07 19:21:37 +0900

    navigation

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -6,6 +6,14 @@ export default function SignUpScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function handlePress() {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'MemoList' }],
+    });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
@@ -32,15 +40,7 @@ export default function SignUpScreen(props) {
           secureTextEntry
           textContentType="password"
         />
-        <Button
-          label="Submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
-        />
+        <Button label="Submit" onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already resistered?</Text>
           <TouchableOpacity
